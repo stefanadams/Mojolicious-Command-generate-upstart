7b733db79231c51136d59c46e35b9e9ddd53122d
Stefan Adams <stefan@cogentinnovators.com>
2014-01-18 20:02:47 -0600

    Deploy to /etc instead of etc

--- lib/Mojolicious/Command/generate/upstart.pm
+++ lib/Mojolicious/Command/generate/upstart.pm
@@ -64,14 +64,14 @@ sub run {
   }
 
   my $file = $deploy
-    ? File::Spec->join('etc', 'init', $self->app->moniker.'.conf')
+    ? File::Spec->join('', 'etc', 'init', $self->app->moniker.'.conf')
     : File::Spec->join($output, 'etc_init_'.$self->app->moniker.'.conf');
   $self->render_to_file('upstart', $file, $self->app);
   $self->chmod_file($file, 0755);
 
   # config file
   $file = $deploy
-    ? File::Spec->join('etc', 'default', $self->app->moniker)
+    ? File::Spec->join('', 'etc', 'default', $self->app->moniker)
     : File::Spec->join($output, 'etc_default_'.$self->app->moniker);
   $self->render_to_file('config', $file, $self->app);
   $self->chmod_file($file, 0644);
